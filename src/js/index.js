511ebe892549d511a6ce83af93df30375969cbde
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2020-11-06 20:17:51 +0100

    Basic functionality JS commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,35 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const key = new Date().toISOString().slice(0, 10); 
+
+const glassOfWaterNumber = document.querySelector('.water-glass__number-js');
+const addButton = document.querySelector('.add-js');
+const substractButton = document.querySelector('.substract-js');
+
+let glassNumber = localStorage.getItem(key);
+if (glassNumber) {
+    console.log(`wartość glassNumber: ${glassNumber}`);
+}
+else{
+    glassNumber = 0;
+}
+
+glassOfWaterNumber.innerHTML = glassNumber;
+
+
+addButton.addEventListener('click', (e) => {
+    glassNumber++;
+    glassOfWaterNumber.innerHTML = glassNumber;
+    localStorage.setItem(key, glassNumber);
+
+});
+
+substractButton.addEventListener('click', (e) => {
+    if(glassNumber > 0) {
+        glassNumber--;
+        glassOfWaterNumber.innerHTML = glassNumber;
+        localStorage.setItem(key, glassNumber);
+    }
+});
